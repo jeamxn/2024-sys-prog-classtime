fff544685ef7179fc19ce1fe6e9bc2e426e1842a
jeamxn <admin@chicken-moo.com>
2024-05-28 16:14:31 +0900

    feat: append_text_to_file()

--- ch5/file_text_low.c
+++ ch5/file_text_low.c
@@ -14,9 +14,24 @@
 #include <unistd.h>
 
 int write_text_to_file(const char *filename, const char *text) {
-  int fd = open(filename, O_WRONLY | O_CREAT, 0644);
+  int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
   if (fd == -1) {
     perror("Error opening file for writing");
+    return 1;
+  }
+  int res = write(fd, text, strlen(text));
+  close(fd);
+  if (res == -1) {
+    perror("Error writing to file");
+    return 1;
+  }
+  return 0;
+}
+
+int append_text_to_file(const char *filename, const char *text) {
+  int fd = open(filename, O_WRONLY | O_APPEND | O_CREAT, 0644);
+  if (fd == -1) {
+    perror("Error opening file for appending");
     return 1;
   }
   int res = write(fd, text, strlen(text));
@@ -49,6 +64,11 @@ int main() {
   const char *text = "Hello, World!";
 
   if (write_text_to_file(filename, text)) {
+    fprintf(stderr, "Text could not be written to file\n");
+    return 1;
+  }
+
+  if (append_text_to_file(filename, "\nHello, again!")) {
     fprintf(stderr, "Text could not be written to file\n");
     return 1;
   }
